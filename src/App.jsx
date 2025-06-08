9f6f0c016c37694cebe02d12378fd3c526429e54
Digdarshan Mohanty <digi@Digdarshans-MacBook-Air.local>
2025-06-08 08:20:46 +0530

    responsive styling

--- src/App.jsx
+++ src/App.jsx
@@ -99,7 +99,7 @@ const App = () => {
     <main>
       <div className="pattern" />
       <div className="wrapper">
-        <header>
+        <header className="min-w-1/3">
           <img src="./hero.png" alt="Hero Banner" />
           <h1>
             Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle
